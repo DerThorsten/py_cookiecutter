0b09e59a4e356fa89dc3a4b5e1384630b1ae3655
DerThorsten <derthorstenbeier@gmail.com>
2019-07-29 15:43:06 +0200

    made tests smarter

---
+++ .ci/test_project.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+echo "------------------------------------------------------------"
+echo "create py_cookiecutter-dev-requirements conda env"
+echo "------------------------------------------------------------"
+conda env create -f py_cookiecutter-dev-requirements.yml || exit 1
+
+
+echo "------------------------------------------------------------"
+echo "activate py_cookiecutter-dev-requirements conda env"
+echo "------------------------------------------------------------"
+$SOURCE activate py_cookiecutter-dev-requirements || exit 1
+
+
+echo "------------------------------------------------------------"
+echo "take one step back"
+echo "------------------------------------------------------------"
+cd .. || exit 1
+
+
+echo "------------------------------------------------------------"
+echo "render coookiecutter template"
+echo "------------------------------------------------------------"
+cookiecutter $TEMPLATE_FOLDER --overwrite-if-exists --no-input || exit 1
+
+
+echo "------------------------------------------------------------"
+echo "go into pytools"
+echo "------------------------------------------------------------"
+cd pytools || exit 1
+
+
+echo "------------------------------------------------------------"
+echo "chmod +x .ci/test_project.sh || exit 1" 
+echo "------------------------------------------------------------"
+chmod +x .ci/test_project.sh || exit 1
+
+
+echo "------------------------------------------------------------"
+echo "./.ci/test_project.sh || exit 1"
+echo "------------------------------------------------------------"
+./.ci/test_project.sh || exit 1
