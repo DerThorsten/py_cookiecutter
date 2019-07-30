c8db870cfdb586d4d68b0cce760dbbb7486ebd2e
DerThorsten <derthorstenbeier@gmail.com>
2019-07-30 16:50:54 +0200

    added missing tests:

---
+++ {{cookiecutter.github_project_name}}/tests/test_version.py
@@ -0,0 +1,8 @@
+import {{cookiecutter.python_package_name}}
+
+
+class TestVersion(object):
+
+    def test_version(self):
+        v = {{cookiecutter.python_package_name}}.__version__
+        assert v == '0.1.0'
