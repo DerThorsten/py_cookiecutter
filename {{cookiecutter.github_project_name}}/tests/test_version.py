ee9dfb4054b150f414baabf94daf7750be4ff50b
DerThorsten <derthorstenbeier@gmail.com>
2019-07-30 16:56:07 +0200

    trigger

--- {{cookiecutter.github_project_name}}/tests/test_version.py
+++ {{cookiecutter.github_project_name}}/tests/test_version.py
@@ -5,4 +5,4 @@ class TestVersion(object):
 
     def test_version(self):
         v = {{cookiecutter.python_package_name}}.__version__
-        assert v == '0.1.0'
+        assert v == {{cookiecutter.version}}
