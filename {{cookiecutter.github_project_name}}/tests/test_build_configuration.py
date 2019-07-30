5db1208774e5ce01ab6f987b348446dc6d333e9c
DerThorsten <derthorstenbeier@gmail.com>
2019-07-30 16:44:30 +0200

    trigger

--- {{cookiecutter.github_project_name}}/tests/test_build_configuration.py
+++
@@ -1,8 +0,0 @@
-import {{cookiecutter.python_package_name}}
-
-
-class TestVersion(object):
-
-    def test_version(self):
-        v = {{cookiecutter.python_package_name}}.__version__
-        assert v == '0.1.0'
